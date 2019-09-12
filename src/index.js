fdb27b56e4a0dea56db2a301b628e5102bfe809a
yuki-well <t.masaki@dotwell.co.jp>
2019-09-12 18:38:26 +0900

    renderSquare修正

--- src/index.js
+++ src/index.js
@@ -12,10 +12,7 @@ class Square extends React.Component {
 
   render() {
     return (
-      <button 
-        className="square" 
-        onClick={() => this.setState({value: 'X'})}
-      >
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
         {this.state.value}
       </button>
     );
@@ -23,8 +20,20 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    return (
+      <Square 
+        value={this.state.squares[i]} 
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
